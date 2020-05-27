693d293f7d6c2c007047f85d8dde3da1e66d232d
WF | Stefan Peshikj <stefan.peshikj@webfactory.mk>
2020-05-27 09:39:55 +0200

    Commit example code

--- README.md
+++ README.md
@@ -1,26 +1,12 @@
 # AnimationBuilder
 
+This example is used to showcase Angular's AnimationBuilder and how it can be used in a simple scenario. Although there are really complex use-cases for the AnimationBuilder, this is just a simple scenario to get started with.
+
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.7.
 
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
-
-## Code scaffolding
-
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
 
 ## Further help
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { animate, animation, AnimationBuilder, AnimationPlayer, group, query, style, useAnimation } from '@angular/animations';
+
+export const someAnimation = animation([
+  group([
+    query('.some-element-selector', [
+      style({ opacity: '0' }),
+      animate('2s', style({ opacity: '1' }))
+    ]),
+    query('.some-other-element-selector', [
+      style({ backgroundColor: 'red', height: '200px' }),
+      animate('2s', style({ backgroundColor: 'white', height: '*' }))
+    ])
+  ])
+]);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'animation-builder';
+export class AppComponent implements OnDestroy {
+  animationPlayer: AnimationPlayer;
+
+  constructor(private animationBuilder: AnimationBuilder) {
+  }
+
+  onStartAnimationClicked(): void {
+    const someOnDoneCallback = () => {
+      console.log('Finished animating');
+    };
+
+    this.createAndStartAnimation(someOnDoneCallback);
+  }
+
+  private createAndStartAnimation(callback: () => void) {
+    const animationFactory = this.animationBuilder.build([
+      useAnimation(someAnimation)
+    ]);
+
+    this.animationPlayer = animationFactory.create(window.document);
+
+    this.animationPlayer.onDone(() => {
+      this.animationPlayer.destroy();
+      this.animationPlayer = null;
+      if (callback) {
+        callback();
+      }
+    });
+
+    this.animationPlayer.play();
+  }
+
+  ngOnDestroy(): void {
+    if (this.animationPlayer) {
+      this.animationPlayer.finish();
+    }
+  }
 }
